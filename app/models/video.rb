c7ef800f12f410315a61996595f69fc7c4fb8079
Gopal <gopal.singh.rails@gmail.com>
2014-11-10 08:51:43 +0530

    added approve/unapprove action for songs and video

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,13 @@
 class Video < ActiveRecord::Base
   belongs_to :videoable, :polymorphic => true
   validates_presence_of :video_url
+  
+  
+    def self.approve_unapprove_video(video_id)
+      videos = Video.where("id in(?)", video_id)
+      videos.each do |video|
+        video.update_attributes(is_approved: !video.is_approved)
+      end  
+    end
+
 end
